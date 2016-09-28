f93e02e3cf6d80985435f8cce830937e6a3f9197
suhao <simplesuhao@163.com>
2016-09-28 22:50:19 +0800

    2016-09-28
    类加载器

--- src/main/java/org/smart4j/framework/ConfigConstant.java
+++ src/main/java/org/smart4j/framework/ConfigConstant.java
@@ -4,9 +4,16 @@ package org.smart4j.framework;
  * @author Sjer
  *
  */
-public class ConfigConstant 
+public interface ConfigConstant 
 {
     String CONFIG_FILE = "smart.properties";
     
+    String JDBC_DRIVER = "smart.framework.jdbc.driver";
+    String JDBC_URL = "smart.framework.jdbc.url";
+    String JDBC_USERNAME = "smart.framework.jdbc.username";
+    String JDBC_PASSWORD = "smart.framework.jdbc.password";
     
+    String APP_BASE_PACKAGE = "smart.framewrok.app.base_package";
+    String APP_JSP_PATH = "smart.framework.app.jsp_path";
+    String APP_ASSET_PATH = "smart.framework.app.asset_paht";
 }
